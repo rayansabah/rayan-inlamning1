c4746de1bac5bb8e73ba09235f30d171ab35c6d9
rayan <raymah0712@skola.goteborg.se>
2021-11-04 15:21:53 +0100

    adding the features

--- run_game.sh
+++ run_game.sh
@@ -1,3 +1,14 @@
-#! / bin / bash
-$echo "rayan"
-echo "rayan"
+#!/bin/bash
+echo "Rayan Sabah Mahmod Program"
+mkdir Rayan_Sabah_labb
+cp Guesser.java  GusserGame.java Rayan_Sabah_labb
+cd Rayan_Sabah_labb
+echo "Running game from" $PWD
+echo compiling...
+echo Running game…
+javac GusserGame.java
+java GusserGame
+echo "Done!"
+echo Removing class files...
+rm  Guesser.class GusserGame.class
+ls
